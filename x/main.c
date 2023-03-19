26f18bf61deeb4798abadb985cfca13a771eaa8c
Ümmü ALTIN <ummualtn2003@gmail.com>
2023-03-19 23:59:53 +0300

    Initial Commit

---
+++ x/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  x
+//
+//  Created by Ümmü Altın on 19.03.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
